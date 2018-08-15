0488d9e36c37548805adb71f635439d0b7ad878c
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2018-08-15 18:04:39 +0600

    update

--- Formula/jpcre2.rb
+++ Formula/jpcre2.rb
@@ -4,7 +4,8 @@ class Jpcre2 < Formula
   url "https://github.com/jpcre2/jpcre2/archive/10.31.02-1.tar.gz"
   sha256 "7228059ea1c72d9d4ff340c417bb4715dcbce23f79c6ed370bd3d1761826ef4a"
   # depends_on "cmake" => :build
-  depends_on :pcre2 => :cxx
+  depends_on :pcre2
+  depends_on :cxx
 
   def install
     system "./configure", "--disable-debug",
