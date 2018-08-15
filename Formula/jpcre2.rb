6d67438d276ada9d191e4ea280b8b32aa90b57b1
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2018-08-15 18:39:48 +0600

    update

--- Formula/jpcre2.rb
+++ Formula/jpcre2.rb
@@ -4,6 +4,7 @@ class Jpcre2 < Formula
   url "https://github.com/jpcre2/jpcre2/archive/10.31.02-1.tar.gz"
   sha256 "7228059ea1c72d9d4ff340c417bb4715dcbce23f79c6ed370bd3d1761826ef4a"
   # depends_on "cmake" => :build
+  revision 1
   depends_on "pcre2"
   version "10.31.02"
 
