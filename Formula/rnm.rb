c047f456adda2f759e1d625d83ad493a8fd691f6
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2018-08-15 19:21:22 +0600

    update

--- Formula/rnm.rb
+++ Formula/rnm.rb
@@ -4,9 +4,9 @@ class Rnm < Formula
   url "https://github.com/neurobin/rnm/archive/4.0.3.tar.gz"
   version "4.0.3"
   sha256 "f9876644dd091a390b2db439a2f3f37115fb42462513fc2e068314c021991153"
-  depends_on "pcre2"
-  depends_on "gmp"
   depends_on "jpcre2" => :build
+  depends_on "gmp"
+  depends_on "pcre2"
 
   def install
     ENV.cxx11
@@ -18,8 +18,8 @@ class Rnm < Formula
   end
 
   test do
-    shell_output("for f in $(seq 1 4000); do touch $f; done").strip()
-    output = shell_output("#{bin}/rnm -y -rs '//fn///fn/@ B16: /idr-b16/ L: /idr-l/ S: /idr-s/ mtime:/info-mtime-%d-%m-%Y %H:%I %p//' -fo -dp -1 -s ./*").strip()
+    shell_output("for f in $(seq 1 4000); do touch $f; done")
+    output = shell_output("#{bin}/rnm -y -rs '//fn///fn/@ B16: /idr-b16/ L: /idr-l/ S: /idr-s/ mtime:/info-mtime-%d-%m-%Y %H:%I %p//' -fo -dp -1 -s ./*").strip
     assert_match "4000", output
   end
 end
