d97bbb121d95ad6562ce10786b59fe77a5aca1da
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2018-10-20 23:38:26 +0600

    update shc formula

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -1,9 +1,9 @@
 class Shc < Formula
   desc "Shell script compiler"
   homepage "https://neurobin.org/projects/softwares/unix/shc/"
-  url "https://github.com/neurobin/shc/archive/3.9.6.tar.gz"
+  url "https://github.com/neurobin/shc/archive/3.9.8.tar.gz"
   version "3.9.6"
-  sha256 "da6a2a3ff4c356a61e086c616561bf681489993cab00c426bad0cfd703a68063"
+  sha256 "12e82886406d3c9ab30fa1a47468c239cea398a0d629c73fe6d438b8fcd5ba1e"
 
   def install
     system "./configure", "--disable-debug",
--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 ```bash
 brew install neurobin/pkg/<package-name>
 ```
+
+
+# Contributing
+
+To create a formula, you can do:
+
+```
+brew create https://github.com/neurobin/shc/archive/<version>.tar.gz
+```
