97ab4f1d4d46438ecb447f158f488fbdd99f9d99
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2018-11-20 14:40:11 +0600

    update shc formula for 4.0.1

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -1,9 +1,9 @@
 class Shc < Formula
   desc "Shell script compiler"
   homepage "https://neurobin.org/projects/softwares/unix/shc/"
-  url "https://github.com/neurobin/shc/archive/4.0.0.tar.gz"
-  version "4.0.0"
-  sha256 "750f84441c45bd589acc3b0f0f71363b0001818156be035da048e1c2f8d6d76b"
+  url "https://github.com/neurobin/shc/archive/4.0.1.tar.gz"
+  version "4.0.1"
+  sha256 "494666df8b28069a7d73b89f79919bdc04e929a176746c98c3544a639978ba52"
 
   def install
     system "./configure", "--disable-debug",
